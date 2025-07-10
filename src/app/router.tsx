6a349349fe6de9b325eebc835774023607aa2324
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-07-10 14:57:10 -0300

    feat: adiciona rotas para as páginas Lumi e LumiTest no roteador

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
+import LumiPage from '@/pages/Lumi'
+import LumiTest from '@/pages/LumiTest'
 
 // Main routes will be added here
 const router = createBrowserRouter([
   {
     path: '/',
     element: <div>Welcome to Toivo</div>,
+  },
+  {
+    path: '/lumi',
+    element: <LumiPage />,
+  },
+  {
+    path: '/lumi-test',
+    element: <LumiTest />,
   },
 ])
 
