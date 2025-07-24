27a96487290d90d1cb289ad03deec72ccef1aec1
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-07-24 11:10:56 -0300

    feat: remove a exportação do useGardenStore do arquivo index.ts, simplificando a estrutura de armazenamento após a remoção de funcionalidades relacionadas a flores

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,3 @@
 export { useKanbanStore } from './kanbanStore';
 export { usePomodoroStore } from './pomodoroStore';
-export { useGardenStore } from './gardenStore';
 export { useLumiStore } from './lumiStore';
